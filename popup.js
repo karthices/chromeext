180e83544a82e5cf9882be1792d814d2c31f16be
karthices <karthices@gmail.com>
2022-11-11 10:22:11 +0530

    chrome extension for login

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 const passwordEle = document.getElementById("password");
 const inputCompanyEle = document.getElementById("setrealestate");
+const Login = document.getElementById("Login");
+
 chrome.storage.sync.get('randpass', function(data) {
     passwordEle.value = data.randpass;
 });
@@ -7,8 +9,7 @@ chrome.storage.sync.get('company', function(data) {
     inputCompanyEle.value = data.company;
 });
 
-// When the button is clicked, inject script into current page
-document.getElementById("Login").addEventListener("click", async() => {
+const loginwithcompany = async() => {
     var companySelected = document.getElementById("setrealestate").value;
     var password = passwordEle.value;
 
@@ -21,17 +22,23 @@ document.getElementById("Login").addEventListener("click", async() => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: setrealestatefn,
+        function: companychange,
         args: [companySelected, password],
     }); // setFawsec().then();
     window.close();
+}
 
-}, false);
+// When the button is clicked, inject script into current page
+Login.addEventListener("click", loginwithcompany, false);
+
+inputCompanyEle.addEventListener("change", loginwithcompany, false);
+
+
 
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setrealestatefn(_id, _password) {
+function companychange(_id, _password) {
     document.getElementById("inputEmail").value = "";
     document.getElementById("inputPassword").value = _password;
     if (document.getElementById("inputCompany"))
